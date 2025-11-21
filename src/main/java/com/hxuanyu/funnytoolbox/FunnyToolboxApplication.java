d70b57d5f4b71a84266988a45f4d73cd8f80083e
hxuanyu <2252193204@qq.com>
2025-11-21 09:39:19 +0800

    项目初始化完成

---
+++ src/main/java/com/hxuanyu/funnytoolbox/FunnyToolboxApplication.java
@@ -0,0 +1,13 @@
+package com.hxuanyu.funnytoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyToolboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunnyToolboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hxuanyu/funnytoolbox/FunnyToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxuanyu.funnytoolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyToolboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
