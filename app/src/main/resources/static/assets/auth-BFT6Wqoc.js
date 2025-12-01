69a788750f9d40016e72da3433a50f35f4dc7bb0
hxuanyu <2252193204@qq.com>
2025-12-01 15:54:57 +0800

    前端页面调整

---
+++ app/src/main/resources/static/assets/auth-BFT6Wqoc.js
@@ -0,0 +1,1 @@
+import{e as o}from"./_plugin-vue_export-helper-DER20O5o.js";const s=t=>o.post("/auth/login",t),u=()=>o.post("/auth/logout");export{u as a,s as l};
--- app/src/main/resources/static/assets/auth-C9Ur-uox.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./_plugin-vue_export-helper-ro2IpPUk.js";const s=t=>o.post("/auth/login",t),u=()=>o.post("/auth/logout");export{u as a,s as l};
